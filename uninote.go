0ecff1e8b312c855829181ab27290e1e494a5907
Igor Brand <igorsca@gmail.com>
2018-06-19 00:05:09 -0300

    Added colors support

--- uninote.go
+++ uninote.go
@@ -9,6 +9,8 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/fatih/color"
 )
 
 var (
@@ -20,28 +22,49 @@ var (
 
 	//Po recebe nota da prova objetiva
 	Po float64
+
+	//Colors
+
+	//Red recebe a cor vermelha
+	Red = color.New(color.FgRed).PrintfFunc()
+
+	//Green recebe a cor verde
+	Green = color.New(color.FgGreen).PrintfFunc()
+
+	//Bwhite recebe a cor bold branco
+	Bwhite = color.New(color.Bold, color.FgWhite).PrintfFunc()
 )
+
+//ColorNota apresenta o resultado da media colorido
+func ColorNota(a float64) float64 {
+	if a < 60 {
+		Red("%v\r\n\n", a)
+	} else if a >= 60 {
+		Green("%v\r\n\n", a)
+	}
+	return a
+}
 
 //Nota das Apol 1,2,3,4 e 5
 func apolMedia(a, b, c, d, e float64) float64 {
 	media := (a + b + c + d + e) / 5
-	fmt.Printf("Sua media das apols foi = %v\r\n\n", media)
-	return media
+	Bwhite("Sua media das apols foi = ")
+	return ColorNota(media)
 }
 
 //Nota da Atividade Pratica + Nota da Prova Discursiva
 func apComPd(a, b float64) float64 {
 	media := (a*4 + b*6) / 10
-	fmt.Printf("Sua media da Prova Discursiva com Atividade Pratica foi = %v\r\n\n", media)
-	return media
+	Bwhite("Sua media da Prova Discursiva com Atividade Pratica foi = ")
+	return ColorNota(media)
 
 }
 
 //Nota da Prova Objetiva + Media das Apols + Media da apComPd
 func mediaGeral(a, b, c float64) float64 {
 	media := (a*30 + b*20 + c*50) / 100
-	fmt.Printf("Sua media Geral nesta materia eh de: %v\r\n\n", media)
-	return media
+	Bwhite("Sua media Geral nesta materia eh de: ")
+	return ColorNota(media)
 }
 
 //Sample reading stdin string with bufio
